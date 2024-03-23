c04a5722c91a04e5e7e3ec92f724b5e2a64ff282
smhrd <smhrd@121.147.52.249>
2024-03-24 00:06:36 +0900

    회원가입 완

---
+++ ToGo_C/src/main/java/kr/smhrd/mapper/MembersMapper_LMT.java
@@ -0,0 +1,14 @@
+package kr.smhrd.mapper;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import kr.smhrd.entity.Members;
+
+@Mapper
+public interface MembersMapper_LMT {
+
+	public void membersInsert(Members members);
+
+	
+
+}
