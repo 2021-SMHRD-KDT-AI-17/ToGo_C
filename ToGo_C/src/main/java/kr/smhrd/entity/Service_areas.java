bedd0640b67a83920caf4d3bfdd8e82c848f4ad8
smhrd <smhrd@121.147.52.249>
2024-03-26 12:45:26 +0900

    service_areas

---
+++ ToGo_C/src/main/java/kr/smhrd/entity/Service_areas.java
@@ -0,0 +1,19 @@
+package kr.smhrd.entity;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Data
+@Getter
+@Setter
+@ToString
+public class Service_areas {
+
+   private String service_idx;
+   private String service_name;
+   private String service_addr;
+   private String lat;
+   private String lng;
+}
