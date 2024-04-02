784057a0378449b2b2dfbaf77dfd955b6ab1e05a
smhrd <smhrd@121.147.52.249>
2024-04-02 22:07:43 +0900

    주문내역 수정중

--- ToGo_C/src/main/java/kr/smhrd/mapper/OrderHistoryMapper.java
+++ ToGo_C/src/main/java/kr/smhrd/mapper/OrderHistoryMapper.java
@@ -3,6 +3,7 @@ package kr.smhrd.mapper;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
 
 import kr.smhrd.entity.OrderHistory;
 
